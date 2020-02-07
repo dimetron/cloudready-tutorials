7f8559dd936e12cc5732b15f4fb36388475991f2
dimetron <dimetron@me.com>
2020-02-07 22:54:25 +0100

    readm me updated

--- digital-ocean/1_vpn_server/environments/vpn/main.tf
+++ digital-ocean/1_vpn_server/environments/vpn/main.tf
@@ -35,6 +35,18 @@ resource "digitalocean_firewall" "vpn-fw" {
   }
 
   inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "udp"
+    port_range       = "1194"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
     protocol         = "udp"
     port_range       = "51820"
     source_addresses = ["0.0.0.0/0", "::/0"]
